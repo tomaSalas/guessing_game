f5b8d81bb0984cfc0034c6eab6b2fbffd5787980
tomaSalas <tomassubero97@gmail.com>
2020-06-12 13:06:03 -0500

    game update

--- guessing_game.py
+++ guessing_game.py
@@ -9,15 +9,21 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 
 """
 
-import random
+from random import randint
+    
 
-
+def lines():
+    print("-" * 40)
+    
+    
 def start_game():
     """Psuedo-code Hints
     
     When the program starts, we want to:
     ------------------------------------
     1. Display an intro/welcome message to the player.
+   
+    
     2. Store a random number as the answer/solution.
     3. Continuously prompt the player for a guess.
       a. If the guess greater than the solution, display to the player "It's lower".
@@ -30,7 +36,38 @@ def start_game():
     ( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
-
+    lines()
+    print("Welcome to Guessing game, made by Tomas!")
+    lines()
+    count = 0
+    ramdom_number  = randint(1, 10)
+    while True:
+        
+        number = int(input("Pick a number between 1 and 10: "))
+        
+        if number == ramdom_number:
+            count += 1
+            print("You got it! It took you {} tries".format(count))
+            answer = input("would you like to play again? (y/n):    ")
+            if answer.lower() == "y":
+                start_game()
+                
+            elif answer.lower() == "n":
+                print("Thank you for playing!")
+                break
+            
+        elif (ramdom_number < number):
+            print("Lower 💁‍♂️")
+            count += 1
+            continue
+            
+        elif (ramdom_number > number):
+            print("Higher 😋")
+            count += 1
+            continue
+        else:
+            print("That is not a valid input, please try again 🙈")
+  
 
 
 # Kick off the program by calling the start_game function.
